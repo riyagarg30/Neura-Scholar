0c0f691629a49265a8ee828353e00b03144c6beb
th-blitz <PreethamRakshith11@gmail.com>
2025-04-29 23:17:33 -0400

    cleaned up ansible bootstraping

--- ansible/scripts.sh
+++ ansible/scripts.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 
-ansible all -i /inventory/myclusters -m ping
+function play_ping() {
+    ansible all -i /inventory/myclusters -m ping
+}
 
 
-function ansible_play() {
-    ansible-playbook -i "/inventory/myclusters" /inventory/pre_k8s/pre_k8s_configure.yml && \
-    ansible-playbook -i "/inventory/myclusters" /inventory/docker-login.yml && \
-    ansible-playbook -i "/inventory/myclusters" cluster.yml --become --become-user=root  && \
+function play_pre_k8s() {
+    ansible-playbook -i "/inventory/myclusters" /inventory/pre_k8s/pre_k8s_configure.yml
+}
+
+function play_build_k8s_cluster() {
+    ansible-playbook -i "/inventory/myclusters/${1}" cluster.yml --become --become-user=root \
+        > "/inventory/${1}.play.logs" 2>&1
+}
+
+function play_post_k8s() {
     ansible-playbook -i "/inventory/myclusters" /inventory/post_k8s/post_k8s_configure.yml
 }
 
-ansible_play > /inventory/play.logs 2>&1
+function play_block_mount() {
+    ansible-playbook -i "/inventory/myclusters/kvm_tacc" /inventory/test/mount.yml
+}
+
