2777db31f3cf48c7903250b00911b1b6b2ca784f
th-blitz <PreethamRakshith11@gmail.com>
2025-05-05 22:33:37 -0400

    tested and working gpu-operator path via ansible

--- terraform/create_reservation.sh
+++ terraform/create_reservation.sh
@@ -8,11 +8,15 @@ function create_reservation() {
     START_DATE=$(date -u +"%Y-%m-%d %H:%M") 
     END_DATE=$(date -u -v+6H +"%Y-%m-%d %H:%M") 
 
-    openstack reservation lease create \
-      --reservation min=1,max=1,resource_type=physical:host,resource_properties='["=", "$node_type", "gpu_rtx_6000"]' \
-      --start-date "$START_DATE" \
-      --end-date "$END_DATE" \
-      "$RESERVATION"
+    output=$(
+        openstack reservation lease create \
+          --reservation min=1,max=1,resource_type=physical:host,resource_properties='["=", "$node_type", "gpu_rtx_6000"]' \
+          --start-date "$START_DATE" \
+          --end-date "$END_DATE" \
+          "$RESERVATION"
+    )
+
+    RESERVATION_ID=$(echo "$output" | grep '"id":' | awk -F'"' '{print $4}' | tail -1)
 
     while true; do
         output=$(openstack reservation lease show "$RESERVATION")
