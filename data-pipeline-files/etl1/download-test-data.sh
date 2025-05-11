8f754a6f4260384a163e6623cb5eaf938e3a3588
Riya Garg <riyagarg30@gmail.com>
2025-05-11 18:40:27 -0400

    other changes

--- data-pipeline-files/etl1/download-test-data.sh
+++ data-pipeline-files/etl1/download-test-data.sh
@@ -1,3 +1,4 @@
-bash run-etl1-test-data.sh retrain_list retrain-data
+chmod +x run-etl1-test-data.sh
+./run-etl1-test-data.sh retrain_list retrain-data
 sleep 2
-bash run-etl1-test-data.sh staging_list staging-data
+./run-etl1-test-data.sh staging_list staging-data
--- data-pipeline-files/etl1/run-etl1-test-data.sh
+++ data-pipeline-files/etl1/run-etl1-test-data.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+filename="${1}.txt"
+data_dir="$2"
+
+echo "file name = $filename"
+echo "data dir = $data_dir"
+
 while IFS= read -r line; do
   echo "file name - ${line}"
-  base=$(basename "$line" .tar)
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = ${2} \
+  DATA_DIRECTORY = ${data_dir} \
   docker compose -f docker-compose-etl1.yaml up extract-data
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = ${2} \
+  DATA_DIRECTORY = ${data_dir} \
   docker compose -f docker-compose-etl1.yaml up transform-data
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = ${2} \
+  DATA_DIRECTORY = ${data_dir} \
   docker compose -f docker-compose-etl1.yaml up load-data
 
-  #docker compose run --rm -e FILE_URL=$line extract-data #| echo "done"   # 2>&1 | tee "logs/$(basename "$line").log"
-done < ${1}.txt
+#   #docker compose run --rm -e FILE_URL=$line extract-data #| echo "done"   # 2>&1 | tee "logs/$(basename "$line").log"
+done < ${filename}
