096e273899729996eb0385675db4ecae797040a3
Riya Garg <riyagarg30@gmail.com>
2025-05-11 18:43:43 -0400

    fix spaces

--- data-pipeline-files/etl1/run-etl1-test-data.sh
+++ data-pipeline-files/etl1/run-etl1-test-data.sh
@@ -10,15 +10,15 @@ while IFS= read -r line; do
   echo "file name - ${line}"
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = ${data_dir} \
+  DATA_DIRECTORY=${data_dir} \
   docker compose -f docker-compose-etl1.yaml up extract-data
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = ${data_dir} \
+  DATA_DIRECTORY=${data_dir} \
   docker compose -f docker-compose-etl1.yaml up transform-data
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = ${data_dir} \
+  DATA_DIRECTORY=${data_dir} \
   docker compose -f docker-compose-etl1.yaml up load-data
 
 #   #docker compose run --rm -e FILE_URL=$line extract-data #| echo "done"   # 2>&1 | tee "logs/$(basename "$line").log"
