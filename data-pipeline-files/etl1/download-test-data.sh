bedc1c3fac4f10ee587c7d0253f18476478ebb72
Riya Garg <riyagarg30@gmail.com>
2025-05-11 18:17:31 -0400

    add code for downloading re-training and staging data

---
+++ data-pipeline-files/etl1/download-test-data.sh
@@ -0,0 +1,3 @@
+bash run-etl1-test-data.sh retrain_list retrain-data
+sleep 2
+bash run-etl1-test-data.sh staging_list staging-data
--- data-pipeline-files/etl1/run-etl1-test-data.sh
+++ data-pipeline-files/etl1/run-etl1-test-data.sh
@@ -2,18 +2,19 @@
 
 while IFS= read -r line; do
   echo "file name - ${line}"
+  base=$(basename "$line" .tar)
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = "raw-data" \
+  DATA_DIRECTORY = ${2} \
   docker compose -f docker-compose-etl1.yaml up extract-data
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = "raw-data" \
+  DATA_DIRECTORY = ${2} \
   docker compose -f docker-compose-etl1.yaml up transform-data
 
   FILE_URL=${line} \
-  DATA_DIRECTORY = "raw-data" \
+  DATA_DIRECTORY = ${2} \
   docker compose -f docker-compose-etl1.yaml up load-data
 
   #docker compose run --rm -e FILE_URL=$line extract-data #| echo "done"   # 2>&1 | tee "logs/$(basename "$line").log"
-done < file_list.txt
+done < ${1}.txt
