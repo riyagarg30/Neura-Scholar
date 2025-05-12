0b9e1d57c6d84b0f52586fbc859a5baacb8529ff
Riya Garg <riyagarg30@gmail.com>
2025-05-11 20:55:12 -0400

    finalise etl scripts for test data

--- data-pipeline-files/etl2/run-etl2.sh
+++ data-pipeline-files/etl2/run-etl2.sh
@@ -6,12 +6,18 @@ while IFS= read -r line; do
   echo "basename - $base"  # Output: arxiv_0705
 
   FILE_URL=${base} \
+  DATA_DIRECTORY="text-files-data" \
+  PDF_DATA_DIRECTORY="raw-data" \
   docker compose -f docker-compose-etl2.yaml up extract-data
 
   FILE_URL=${base} \
+  DATA_DIRECTORY="text-files-data" \
+  PDF_DATA_DIRECTORY="raw-data" \
   docker compose -f docker-compose-etl2.yaml up transform-data
 
   FILE_URL=${base} \
+  DATA_DIRECTORY="text-files-data" \
+  PDF_DATA_DIRECTORY="raw-data" \
   docker compose -f docker-compose-etl2.yaml up load-data
 
 done < tar_files_list.txt
