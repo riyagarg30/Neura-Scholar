db92e9bfa1885521b26c96c71e981e3d17594803
Riya Garg <riyagarg30@gmail.com>
2025-05-12 18:14:06 -0400

    fix path

--- data-pipeline-files/etl4/run-etl4-test-data.sh
+++ data-pipeline-files/etl4/run-etl4-test-data.sh
@@ -7,6 +7,6 @@ CHUNKS_DATA_TABLE=${3} \
 docker compose -f docker-compose-etl4.yaml up transform-data
 
 docker rm etl_4_extract_data
-docker etl_4_transform_data
+docker rm etl_4_transform_data
 
 docker volume rm clean-meta-data_raw-data
